7452f7e59dec5d564e6029c350e75fb29e9e4428
ParASHute <119108596+ParASHute@users.noreply.github.com>
2024-04-09 17:00:40 +0900

    Item, SpecialItem Class  생성

--- DX_Portfolio/Framework/GameSources/Classes/Item/SpecialItem.h
+++ DX_Portfolio/Framework/GameSources/Classes/Item/SpecialItem.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include "Skeleton/TextureRect.h"
+
+enum class ItemType {
+	HEART = 0,
+	ARROW,
+	BOME,
+	EMPTY_BOTTLE,
+	FULL_BOTTLE
+};
+
+class Item {
+public:
+	Item(Vector3 position, Vector3 size, float rotatioin = 0.0f);
+	~Item();
+
+	void SetImage(ItemType type);
+
+	ItemType GetType() { return type; }
+	int GetItemCount() { return  itemCount; }
+
+	void SetType(ItemType type) { this->type = type; }
+	int SetItemCount(int itemCount) { this->itemCount = itemCount; }
+
+private:
+	TextureRect image;
+
+	ItemType type;
+	int itemCount;
+};
